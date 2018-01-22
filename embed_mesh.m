1f857564a518c2b41e1a24f78565bcaf147ff7d8
Noam Aigerman <aigerman@adobe.com>
2018-01-22 15:23:41 -0800

    cleanup

--- embed_mesh.m
+++ embed_mesh.m
@@ -2,10 +2,10 @@ function [flattener] = embed_mesh(V,T,inds,isdisc)
 flattener=Flattener(V,T,inds,'isdisc',isdisc);
 flattener.mesh_name='mesh';
 if isdisc
-flattener.disk_orbifold();
+    flattener.disk_orbifold();
 else
+    flattener.orderTS();
     flattener.flatten_orbifold(true);
 end
-% flattener.computeDistortion(true);
 end
 
